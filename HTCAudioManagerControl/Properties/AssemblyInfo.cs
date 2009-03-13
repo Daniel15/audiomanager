a3746a112391b0e58608c3cba4458fed28bb5859
daniel@d15.biz <daniel@d15.biz@821f2432-0faa-11de-ab22-55a8a853a75f>
2009-03-13 08:50:40 +0000

    Initial import

---
+++ HTCAudioManagerControl/Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HTC AudioManager Control")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Daniel15")]
+[assembly: AssemblyProduct("HTCAudioManagerControl.Properties")]
+[assembly: AssemblyCopyright("2009 Daniel15 (http://d15.biz/)")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3a87aee2-36e5-435e-a802-065dba4fdd49")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("0.0.1.1")]
