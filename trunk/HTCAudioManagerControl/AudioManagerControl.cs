a00fd1b74484d48ea1c33b7c8cbee85527e5daab
daniel@d15.biz <daniel@d15.biz@821f2432-0faa-11de-ab22-55a8a853a75f>
2009-03-15 03:28:21 +0000

    Should hopefully fix issues with older phones.

--- trunk/HTCAudioManagerControl/AudioManagerControl.cs
+++ trunk/HTCAudioManagerControl/AudioManagerControl.cs
@@ -1,5 +1,5 @@
 ﻿/**
- * HTC AudioManager Controll
+ * HTC AudioManager Control
  * By Daniel15 - http://d15.biz/
  */
 using System;
@@ -44,7 +44,12 @@ namespace HTCAudioManagerControl
 		{
 			try
 			{
+				// For newer devices (Touch Pro, etc.)
 				_handle = FindWindowCE("AUDIO_MANAGER_ENG", null);
+				// If nothing, it might be an older one.
+				if (_handle == IntPtr.Zero)
+					_handle = FindWindowCE("AUDIO_MANAGER", null);
+				// Still nothing?
 				if (_handle == IntPtr.Zero)
 					throw new Exception("Cannot find AudioManager window");
 
